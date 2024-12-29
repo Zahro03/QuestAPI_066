650d89b1196adec68d64f0476698e4630c01d583
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-29 10:33:30 +0700

    mengimplementasikan interface atau kelas DestinasiNavigasi

---
+++ app/src/main/java/com/example/meet13/ui/view/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.meet13.ui.view
+
+import com.example.meet13.navigation.DestinasiNavigasi
+
+object DestinasiHome:DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
