5cc4c83b339fbbb55336f07624b7c8eed06fbfe2
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-29 10:44:56 +0700

    membuat layar dengan bagian atas (header) yang bisa ikut bergerak saat layar digeser, dan ada tombol untuk memperbarui data mahasiswa.

--- app/src/main/java/com/example/meet13/ui/view/HomeView.kt
+++ app/src/main/java/com/example/meet13/ui/view/HomeView.kt
@@ -1,9 +1,13 @@
 package com.example.meet13.ui.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet13.costumWidget.CostumeTopAppBar
 import com.example.meet13.navigation.DestinasiNavigasi
 import com.example.meet13.ui.viewModel.HomeViewModel
 
@@ -20,5 +24,18 @@ fun HomeScreen(
     onDetailClick: (String) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+    )
 }
