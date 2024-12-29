78eea4e1da4962ecd733cc9e7f028005ca839a61
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-29 10:55:57 +0700

    menampilkan data mahasiswa dengan opsi untuk melihat detail dan menghapus mahasiswa.

--- app/src/main/java/com/example/meet13/ui/view/HomeView.kt
+++ app/src/main/java/com/example/meet13/ui/view/HomeView.kt
@@ -1,8 +1,11 @@
 package com.example.meet13.ui.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -47,8 +50,17 @@ fun HomeScreen(
                 shape = MaterialTheme.shapes.medium,
                 modifier = Modifier.padding(18.dp)
             ) {
-
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Mahasiswa")
             }
-        }
-    )
+        },
+    ){ innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUiState,
+            retryAction = {viewModel.getMhs()},modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,onDeleteClick = {
+                viewModel.deleteMhs(it.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
 }
