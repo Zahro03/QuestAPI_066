e96c5f7c034c7c2a24c91545028c19bee4f1e170
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-29 10:37:39 +0700

    untuk menampilkan UI dan mengatur interaksi pada layar beranda

--- app/src/main/java/com/example/meet13/ui/view/HomeView.kt
+++ app/src/main/java/com/example/meet13/ui/view/HomeView.kt
@@ -1,9 +1,24 @@
 package com.example.meet13.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet13.navigation.DestinasiNavigasi
+import com.example.meet13.ui.viewModel.HomeViewModel
 
 object DestinasiHome:DestinasiNavigasi{
     override val route = "home"
     override val titleRes = "Home Mhs"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
