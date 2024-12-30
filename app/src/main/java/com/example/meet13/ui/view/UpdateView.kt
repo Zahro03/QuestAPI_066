b8c96e28f7bc374ed14a8b84b4e33a99c441a7a7
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-30 11:15:36 +0700

    finish

--- app/src/main/java/com/example/meet13/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/meet13/ui/view/UpdateView.kt
@@ -1,15 +1,21 @@
 package com.example.meet13.ui.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.meet13.costumWidget.CostumeTopAppBar
 import com.example.meet13.navigation.DestinasiNavigasi
 import com.example.meet13.ui.viewModel.PenyediaViewModel
+import com.example.meet13.ui.viewModel.UpdateViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiUpdate : DestinasiNavigasi{
     override val route = "Update_mhs"
-    const val titleRes = "Edit Mahasiswa"
+    override val titleRes= "Edit Mahasiswa"
     const val NIM = "nim"
     val routeWithArgs = "$route/{$NIM}"
 }
@@ -22,5 +28,27 @@ fun UpdateMhsScreen(
     modifier: Modifier = Modifier,
     updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = "Update Mahasiswa",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = updateViewModel.updateMhsUiState,
+            onSiswaValueChange = updateViewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    updateViewModel.updateMhs()
+                    onNavigateUp()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
