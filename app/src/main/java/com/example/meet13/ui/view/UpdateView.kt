3f2c37209f9871b00c624d82c271137e2ddec925
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-30 08:20:35 +0700

    halaman untuk mengedit data mahasiswa, dengan fungsionalitas navigasi kembali dan pembaruan data mahasiswa menggunakan ViewMode

--- app/src/main/java/com/example/meet13/ui/view/UpdateView.kt
+++ app/src/main/java/com/example/meet13/ui/view/UpdateView.kt
@@ -1,10 +1,26 @@
 package com.example.meet13.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.meet13.navigation.DestinasiNavigasi
+import com.example.meet13.ui.viewModel.PenyediaViewModel
 
 object DestinasiUpdate : DestinasiNavigasi{
     override val route = "Update_mhs"
     const val titleRes = "Edit Mahasiswa"
     const val NIM = "nim"
     val routeWithArgs = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateMhsScreen(
+    navigateBack:() -> Unit,
+    onNavigateUp:() -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
