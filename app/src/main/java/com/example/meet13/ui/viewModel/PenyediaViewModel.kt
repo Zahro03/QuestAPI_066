06a64a21a6ee1e21cf386cfacc68ae3baa6b0f8b
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-12-30 11:21:47 +0700

    Menambahkan inisialisasi Detail sama Update

--- app/src/main/java/com/example/meet13/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/meet13/ui/viewModel/PenyediaViewModel.kt
@@ -1,16 +1,40 @@
 package com.example.meet13.ui.viewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.meet13.MahasiswaApplications
 
-object PenyediaViewModel{
+object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
-        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        // Inisialisasi untuk HomeViewModel
+        initializer {
+            HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+
+        // Inisialisasi untuk InsertViewModel
+        initializer {
+            InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+
+        // Inisialisasi untuk DetailViewModel
+        initializer {
+            DetailViewModel(createSavedStateHandle(), aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
+
+        // Inisialisasi untuk UpdateViewModel
+        initializer {
+            UpdateViewModel(createSavedStateHandle(), aplikasiMahasiswa().container.mahasiswaRepository
+            )
+        }
     }
+    }
+
+    // Fungsi untuk mengakses MahasiswaApplications dari CreationExtras
     fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
         (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
-}
